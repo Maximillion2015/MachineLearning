19fb6d0a038f50fd54d23b2d60c7eaa3080d109b
张艺峰 <zhangyifeng@bytedance.com>
2020-01-19 16:20:25 +0800

    add AiLearning

---
+++ MyMl/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/19 4:02 下午
+# @Author  : zhangyifeng
+# @FileName: __init__.py.py
+# @Software: PyCharm
---
+++ db_to_data.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/19 3:41 下午
+# @Author  : zhangyifeng
+# @FileName: db_to_data.py
+# @Software: PyCharm
+
+
+
